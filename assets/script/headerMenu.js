149b7cb262a9af0d14d7bcb24ce96aea42959612
Fura-Crew <109653577+Fura-Crew@users.noreply.github.com>
2023-09-17 14:50:23 +0500

    Add files via upload

---
+++ assets/script/headerMenu.js
@@ -0,0 +1,8 @@
+const MenuBlock = document.getElementById('mobile-menu-block')
+const MenuButton = document.getElementById('mobile-menu__button')
+const MenuList = document.getElementById('mobile-menu-nav-block')
+
+MenuButton.addEventListener('click', function(){
+  MenuButton.classList.toggle('mobile-menu__button__close')
+  MenuList.classList.toggle('open-list')
+})

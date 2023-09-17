a262f19cba04d3e546aa724b8d1ecbc5bbe64287
Fura-Crew <109653577+Fura-Crew@users.noreply.github.com>
2023-09-17 14:54:24 +0500

    Update headerMenu.js

--- assets/script/headerMenu.js
+++ assets/script/headerMenu.js
@@ -5,4 +5,5 @@ const MenuList = document.getElementById('mobile-menu-nav-block')
 MenuButton.addEventListener('click', function(){
   MenuButton.classList.toggle('mobile-menu__button__close')
   MenuList.classList.toggle('open-list')
+  console.log("click!")
 })
